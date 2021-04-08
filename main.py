49a293caa798086a88bb1dd06896ac4c305ce29b
Xu Ruijun <1687701765@qq.com>
2021-04-09 00:28:34 +0800

    feat: USBDevice, add test code for STM32

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from drivers.usbd import USBDevice
+from parsers.dataparser import ArrayParser
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    usbd = USBDevice(idVendor=0xffff)
+    ap = ArrayParser(usbd, 100, 8, np.uint16, 64)
+    ap.start()
+    while True:
+        frame = ap.arr_buf.get_frames(1)
+        print(frame)
